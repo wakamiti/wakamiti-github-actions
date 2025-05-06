b3f4d72842cd68c854e2b7dc1cf62a2cb111b71e
Maria Galbis <mgalbis@iti.es>
2025-05-06 23:01:58 +0200

    feat: update initialization scripts for user configuration and repository setup

--- services/scripts/sonatype_init.sh
+++ services/scripts/sonatype_init.sh
@@ -2,7 +2,22 @@
 
 ./nexus/bin/nexus run &
 while ! curl -sSf http://localhost:8081/service/rest/v1/status > /dev/null; do sleep 10; done
-curl -u admin:admin123 -X POST 'http://localhost:8081/service/rest/v1/repositories/maven/hosted' \
+
+# Cambiar el nombre de usuario del administrador a 'tester'
+curl -u admin:xK9#mP2@ -X PUT 'http://localhost:8081/service/rest/v1/security/users/admin' \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "userId": "tester",
+    "firstName": "Tester",
+    "lastName": "User",
+    "emailAddress": "tester@example.com",
+    "password": "xK9#mP2@",
+    "status": "active",
+    "roles": ["nx-admin"]
+  }'
+
+# Crear repositorios
+curl -u tester:xK9#mP2@ -X POST 'http://localhost:8081/service/rest/v1/repositories/maven/hosted' \
   -H 'Content-Type: application/json' \
   -d '{
     "name": "maven-internal",
@@ -26,7 +41,7 @@ curl -u admin:admin123 -X POST 'http://localhost:8081/service/rest/v1/repositori
       "contentDisposition": "ATTACHMENT"
     }
   }'
-curl -u admin:admin123 -X POST 'http://localhost:8081/service/rest/v1/repositories/maven/hosted' \
+curl -u tester:xK9#mP2@ -X POST 'http://localhost:8081/service/rest/v1/repositories/maven/hosted' \
   -H 'Content-Type: application/json' \
   -d '{
     "name": "maven-snapshots",
